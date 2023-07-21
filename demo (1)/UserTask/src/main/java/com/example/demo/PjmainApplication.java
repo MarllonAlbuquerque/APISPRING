c126f4f82168ffab7ec661736ca56cbc248af61c
Marllon Albuquerque <marllonalb@gmail.com>
2023-07-21 18:14:19 -0300

    first

---
+++ demo (1)/UserTask/src/main/java/com/example/demo/PjmainApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjmainApplication.class, args);
+	}
+
+}
